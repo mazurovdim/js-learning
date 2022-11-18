be482af635a89aa0d108bc1ced09f8fd751e00b3
Dmitriy Mazurov <mazurovdimon@gmail.com>
2022-11-18 11:45:21 +0300

    Создана первичная внешняя часть приложения и начало работы логики

---
+++ 005. Асинхронность в браузере/kolobok/index-old.js
@@ -0,0 +1,35 @@
+
+async function createSlide(slideName){ 
+    const slide = document.createElement('div') 
+    const slideImage = document.createElement('img') 
+    const slideText = document.createElement('p') 
+    const slidePromiseText = await fetch(`data/texts/${slideName}.txt`).then(data => data.text()) 
+ 
+    slideImage.src = `data/images/${slideName}.jpg`
+    slideText.textContent = slidePromiseText 
+    slide.appendChild(slideImage) 
+    slide.appendChild(slideText)
+    slide.classList.add('slide')
+    slide.classList.add('faded-card')
+    return slide
+} 
+
+function funcDelay(ms, callback){
+    return new Promise(res => {
+        setTimeout(() => res(callback), ms)
+    })
+}
+
+async function showslide(){
+/*  Promise.all(['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth'].map(el => createSlide(el)))
+    .then(bls => bls.forEach((bl, index) => {
+        setTimeout(() => {document.querySelector('.content-wrapper').appendChild(bl)}, index * 2000)
+    }))*/
+    let slidesPromisMass = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth'].map(el => createSlide(el))
+    for(const slide of slidesPromisMass){
+        await funcDelay(2000, slide).then(sl => document.querySelector('.content-wrapper').appendChild(sl))
+    }
+}
+
+showslide()
+
--- 005. Асинхронность в браузере/kolobok/index.js
+++ 005. Асинхронность в браузере/kolobok/index.js
@@ -1,35 +1,20 @@
+let currentSlide = 0
+let sections //= Array.from(document.querySelectorAll('section')) Создаются асинхронно
+const nextButton = document.querySelector('.next-slide')
+const prevButton = document.querySelector('.prev-slide') 
 
-async function createSlide(slideName){ 
-    const slide = document.createElement('div') 
-    const slideImage = document.createElement('img') 
-    const slideText = document.createElement('p') 
-    const slidePromiseText = await fetch(`data/texts/${slideName}.txt`).then(data => data.text()) 
- 
-    slideImage.src = `data/images/${slideName}.jpg`
-    slideText.textContent = slidePromiseText 
-    slide.appendChild(slideImage) 
-    slide.appendChild(slideText)
-    slide.classList.add('slide')
-    slide.classList.add('faded-card')
-    return slide
-} 
-
-function funcDelay(ms, callback){
-    return new Promise(res => {
-        setTimeout(() => res(callback), ms)
-    })
+function showSlide(){
+    sections.forEach(section => section.classList.add('hidden'))
+    sections[currentSlide].classList.remove('hidden')
 }
 
-async function showslide(){
-/*  Promise.all(['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth'].map(el => createSlide(el)))
-    .then(bls => bls.forEach((bl, index) => {
-        setTimeout(() => {document.querySelector('.content-wrapper').appendChild(bl)}, index * 2000)
-    }))*/
-    let slidesPromisMass = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth'].map(el => createSlide(el))
-    for(const slide of slidesPromisMass){
-        await funcDelay(2000, slide).then(sl => document.querySelector('.content-wrapper').appendChild(sl))
-    }
+function showNextSlide(){
+    console.log('next')
+    if(currentSlide === sections.length - 1) return
+    currentSlide++
+    showSlide()
 }
 
-showslide()
+nextButton.addEventListener('click', showNextSlide)
 
+//TODO Создать динамическое создание секций внутри div tale
